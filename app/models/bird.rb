f2bedc56c0f2a35264a590c22f3e87b1832aa0f5
Ronaldkubasu <ronald.kubasu@student.moringaschool.com>
2022-11-30 08:06:41 +0300

    Second attempt

--- app/models/bird.rb
+++ app/models/bird.rb
@@ -1,2 +1,11 @@
 class Bird < ApplicationRecord
+    def bird_params
+        params.permit(:name, :species, :likes)
+      end
 end
+
+class AddLikesToBird < ActiveRecord::Migration[6.1]
+    def change
+      add_column :birds, :likes, :integer, null: false, default: 0
+    end
+  end
